7c3148b18c6b59246e75f545b196c227efafe613
Admiral <pooyatavan@gmail.com>
2025-12-10 08:42:16 +0330

    Update and rename Export.sh1 to Export.ps1

--- Script/Export/Export.ps1
+++ Script/Export/Export.ps1
@@ -1,5 +1,5 @@
 $filename = $Env:ComputerName + ".json"
-$OutDir = "C:\Users\" + (Get-ChildItem Env:USERNAME).Value + "\Dropbox"
+$OutDir = "C:\Users\" + (Get-ChildItem Env:USERNAME).Value + "\OneDrive"
 New-Item -ItemType Directory -Force -Path $OutDir | Out-Null
 $path = Join-Path $OutDir $filename
 $awUrl = "http://localhost:5600/api/0/export"
